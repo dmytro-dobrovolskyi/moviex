3f46f8fde294ee3c1c37fbcea0ce289096e6ea30
DmytroDobrovolskyi <gwini777@gmail.com>
2015-08-14 17:43:43 +0300

    Change welcome page

---
+++ presentation/src/main/webapp/static/scripts/main.js
@@ -0,0 +1,15 @@
+require.config({
+
+    paths: {
+        'domReady': '../lib/requirejs-domready/domReady',
+        'angular': '../lib/angular/angular'
+    },
+
+    shim: {
+        'angular': {
+            exports: 'angular'
+        }
+    },
+
+    deps: ['./bootstrap']
+});
