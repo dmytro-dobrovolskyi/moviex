a30ab87252b9be095373cb2c4998dd4e9261ad9f
dmytro-dobrovolskyi <dobrovolskyi.dmytro@gmail.com>
2016-04-04 19:07:01 +0300

    Fix $tooltipProvider injection

--- web/src/main/webapp/client/src/main.js
+++ web/src/main/webapp/client/src/main.js
@@ -1,29 +1,69 @@
-(function (define, angular) {
+(function (define, require) {
 
     "use strict";
+    /**
+     * Defines alias in 'path' and no-AMD dependencies in 'shim'.
+     */
+    require.config({
+        paths: {
+            "angular": "../lib/angular/angular",
+            "uiRouter": "../lib/angular-ui-router/release/angular-ui-router.min",
+            "ngBootstrap": "../lib/angular-bootstrap/ui-bootstrap-tpls.min",
+            "ngResource": "../lib/angular-resource/angular-resource.min",
+            "ngSpingHateoas": "../lib/angular-spring-data-rest/dist/angular-spring-data-rest.min",
+            "ngAnimate": "../lib/angular-animate/angular-animate.min"
+        },
+        shim: {
+            "uiRouter": {
+                "deps": ["angular"]
+            },
+            "ngBootstrap": {
+                "deps": ["angular"]
+            },
+            "ngResource": {
+                "deps": ["angular"]
+            },
+            "ngSpingHateoas": {
+                "deps": ["angular"]
+            },
+            "ngAnimate": {
+                "deps": ["angular"]
+            }
+        }
+    });
 
-    define(
+    require
+    (
         [
-            "start/StartModule",
-            "movie/MovieModule"
+            "uiRouter",
+            "ngBootstrap",
+            "ngResource",
+            "ngSpingHateoas",
+            "ngAnimate"
         ],
-        function (StartModule, MovieModule) {
-
-            var appName = "moviex";
-            var app = angular.module(appName,
+        function () {
+            require(
                 [
-                    "ui.router",
-                    "ui.bootstrap",
-                    "ngResource",
-                    "ngAnimate",
-                    "spring-data-rest",
-                    StartModule,
-                    MovieModule
-                ])
-                .config(['$tooltipProvider', function ($tooltipProvider) {
-                    $tooltipProvider.options({animation: false});
-                }]);
-            return appName;
+                    "start/StartModule",
+                    "movie/MovieModule"
+                ],
+                function (StartModule, MovieModule) {
+                    var appName = "moviex";
+                    angular.module(appName,
+                        [
+                            "ui.router",
+                            "ui.bootstrap",
+                            "ngResource",
+                            "ngAnimate",
+                            "spring-data-rest",
+                            StartModule,
+                            MovieModule
+                        ])
+                        .config(['$uibTooltipProvider', function ($uibTooltipProvider) {
+                            $uibTooltipProvider.options({animation: false});
+                        }]);
+                    angular.bootstrap(document.getElementById(appName), [appName]);
+                })
         }
     );
-})(define, angular);
+})(define, require);
